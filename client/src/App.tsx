7553acb5205440cf4d04f22c29f06cdcd12cd9b6
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-05-07 19:38:54 +0330

    updated landing and added new vector to the section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,17 +6,11 @@ import {
   Welcome,
   Landing,
 } from "./components/index.ts";
-import LandingSVG from "./images/landing.svg";
 
 function App() {
   return (
     <div className="min-h-screen">
       <div className="gradient-bg-welcom bg-[#0f0e13] flex justify-center flex-col items-center">
-        <img
-          className="absolute right-0 top-0 mf:top-[-100px] z-0"
-          src={LandingSVG}
-          alt=""
-        />
         <Navbar />
         <Landing />
         <Welcome />
--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -1,23 +1,32 @@
+import Spline from "@splinetool/react-spline";
 import { AiFillPlayCircle } from "react-icons/ai";
 
 const Landing = () => {
   return (
     <div className="h-[calc(100vh-64px)] flex items-center flex-col w-full">
-      <div className="flex flex-col items-center justify-center gap-12 h-4/5">
-        <h1 className="text-5xl sm:text-7xl py-1 text-center text-white">
-          <b className="text-grad-2">Unleash</b> <br />{" "}
-          <span className="text-grad-1">Virtual Coins.</span>
-        </h1>
-        <a
-          href="#welcome"
-          className="cursor-pointer flex flex-row justify-center items-center my-5 border-2 border-gray-300 hover:bg-gray-300 hover:text-black text-gray-100 text p-3 rounded-full transition-all"
-        >
-          <AiFillPlayCircle className=" mr-2" />
-          <p className=" text-base font-semibold">Create Transaction</p>
-        </a>
+      <div className="flex flex-col mf:flex-row items-center justify-center h-4/5 w-full mf:gap-16">
+        <div className="order-1 mf:order-0">
+          <h1 className="text-5xl sm:text-7xl py-1 text-center text-white">
+            <b className="text-grad-2">Unleash</b> <br />{" "}
+            <span className="text-grad-1">Virtual Coins.</span>
+          </h1>
+          <a
+            href="#welcome"
+            className="cursor-pointer flex flex-row justify-center items-center my-5 border-2 border-gray-300 hover:bg-gray-300 hover:text-black text-gray-100 text p-3 rounded-full transition-all"
+          >
+            <AiFillPlayCircle className=" mr-2" />
+            <p className=" text-base font-semibold">Create Transaction</p>
+          </a>
+        </div>
+        <div className="mf:order-1 w-[250px] h-[350px] mf:w-[400px] mf:h-[500px] flex items-center justify-center relative">
+          <Spline
+            className=" w-full"
+            scene="https://prod.spline.design/HspFWhBBPqzPTJY3/scene.splinecode"
+          />
+        </div>
       </div>
-      <div className="white-glassmorphism rounded-none border-0 mf:border mf:border-x-0 w-full p-4 flex justify-center ">
-        <ul className=" w-full mf:w-2/3 flex justify-around flex-wrap mf:flex-nowrap ">
+      <div className=" w-full p-4 flex justify-center ">
+        <ul className=" w-full mf:w-2/3 flex justify-around flex-wrap mf:flex-nowrap white-glassmorphism py-4 px-2 border-none">
           <div className="flex w-full gap-x-3">
             <li className="pb-2 mf:pb-0 border-b-2 mf:border-b-0 mf:border-r-2 border-[#a3a3a338] text-center text-gray-100 px-6 flex-1 flex flex-col">
               <span className="text-gray-300 text-xs tracking-widest -rotate-2">
